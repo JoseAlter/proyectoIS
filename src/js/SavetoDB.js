eea582f0f4d4f9ca8c51ce6d158de913d7f535e4
augusto pacheco <augustop407@gmail.com>
2020-05-28 23:04:21 -0600

    cambios para hacer get

--- src/js/SavetoDB.js
+++ src/js/SavetoDB.js
@@ -6,7 +6,7 @@ const AWS = require('aws-sdk');
 AWS.config.setPromisesDependency(require('bluebird'));
 const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
-
+//post a dynamo
 module.exports.submit = (event, context, callback) => {
   console.info(event)
   let data = JSON.parse(event.body)
@@ -54,4 +54,28 @@ const cadidatedata = (Objects) => {
   const anidado = Object.assign(Objects, datos)
   return anidado
 }
+/////get por id 
 
+//URL: https://01kq0x6arl.execute-api.us-east-1.amazonaws.com/dev/contratos/{id}
+module.exports.get = (event, context, callback) => {
+  const params = {
+    TableName: process.env.CANDIDATE_TABLE,
+    Key: {
+      id: event.pathParameters.id,
+    },
+  };
+
+  dynamoDB.get(params).promise()
+    .then(result => {
+      const response = {
+        statusCode: 200,
+        body: JSON.stringify(result.Item),
+      };
+      callback(null, response);
+    })
+    .catch(error => {
+      console.error(error);
+      callback(new Error('Couldn\'t fetch contract'));
+      return;
+    });
+};
